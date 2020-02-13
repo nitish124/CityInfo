0583e2eb2dd97d1f026cd23369b4b899bade99db
nitish <nitish124@live.com>
2020-02-13 16:14:19 +0400

    Small changes

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -20,5 +20,6 @@ namespace CityInfo.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
